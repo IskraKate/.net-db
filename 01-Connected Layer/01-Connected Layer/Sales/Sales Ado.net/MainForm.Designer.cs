dff1f7655862598d02c9ecf7e30d5848daa84949
Iskra Kate <kateiskra99@gmail.com>
2020-03-24 14:15:42 +0200

    Connected layer hw-01 Sales was added

---
+++ 01-Connected Layer/01-Connected Layer/Sales/Sales Ado.net/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Sales
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.BFirstname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BLastname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SFirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SLastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MoneySum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // BFirstname
+            // 
+            this.BFirstname.Text = "Buyer`s Name";
+            this.BFirstname.Width = 145;
+            // 
+            // BLastname
+            // 
+            this.BLastname.Text = "Buyer`s LastName";
+            this.BLastname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BLastname.Width = 145;
+            // 
+            // SFirstName
+            // 
+            this.SFirstName.Text = "Saler`s Name";
+            this.SFirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.SFirstName.Width = 145;
+            // 
+            // SLastName
+            // 
+            this.SLastName.Text = "Saler`s Lastname";
+            this.SLastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.SLastName.Width = 145;
+            // 
+            // MoneySum
+            // 
+            this.MoneySum.Text = "Money Sum";
+            this.MoneySum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.MoneySum.Width = 100;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Date.Width = 120;
+            // 
+            // listView
+            // 
+            this.listView.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.listView.AutoArrange = false;
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.BFirstname,
+            this.BLastname,
+            this.SFirstName,
+            this.SLastName,
+            this.MoneySum,
+            this.Date});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Scrollable = false;
+            this.listView.Size = new System.Drawing.Size(800, 450);
+            this.listView.TabIndex = 2;
+            this.listView.TabStop = false;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sales";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader BFirstname;
+        private System.Windows.Forms.ColumnHeader BLastname;
+        private System.Windows.Forms.ColumnHeader SFirstName;
+        private System.Windows.Forms.ColumnHeader SLastName;
+        private System.Windows.Forms.ColumnHeader MoneySum;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ 01-Connected Layer/01-Connected Layer/Sales/Sales.sql
@@ -0,0 +1,74 @@
+IF DB_ID('Sales') IS NOT NULL
+BEGIN
+	USE master
+	 ALTER DATABASE Sales SET single_user with rollback immediate
+    DROP DATABASE Sales
+END
+GO 
+
+CREATE DATABASE Sales
+GO
+
+USE Sales
+Go
+
+CREATE TABLE Buyers
+(
+	Id int NOT NULL PRIMARY KEY,
+	FirstName varchar(25),
+	LastName varchar(50)
+)
+Go
+
+CREATE TABLE Salers
+(
+	Id int NOT NULL PRIMARY KEY,
+	FirstName varchar(25),
+	LastName varchar(50)
+)
+Go
+
+CREATE TABLE Sales
+(
+	Id int NOT NULL PRIMARY KEY,
+	BuyerFk int,
+	SalerFk int,
+	MoneySum int,
+	[Date]  date
+)
+Go
+
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (1, 'Шамиль',	'Власов')
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (2, 'Стефан',	'Лебедев')
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (3, 'Ярослав',	'Пономаренко')
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (4, 'Борис',		'Ильин')
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (5, 'Орландо',	'Юдин')
+INSERT INTO Buyers(Id, FirstName, LastName) VALUES (6, 'Егор',		'Зайцев')
+
+
+ 
+Наумов Ростислав 
+Рыбаков Зиновий
+Шамрыло Платон 
+Чикольба Сергей 
+Василенко Жерар 
+ Зенон 
+
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (1, 'Ростислав',	 'Наумов')
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (2, 'Зиновий',	 'Рыбаков')
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (3, 'Платон',	 'Шамрыло')
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (4, 'Сергей',	 'Чикольба')
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (5, 'Жерар',		 'Василенко')
+INSERT INTO Salers(Id, FirstName, LastName) VALUES (6, 'Зенон',		 'Коломоец')
+
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (1, 6,	1, 100, '2020-04-03')
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (2, 5,	2, 250, '2020-03-24')
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (3, 4,	3, 300, '2020-03-23')
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (4, 3,	4, 350, '2020-03-21')
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (5, 2,	5, 400, '2020-03-22')
+INSERT INTO Sales(Id, BuyerFk, SalerFk, MoneySum, [Date]) VALUES (6, 1,	6, 500, '2020-03-22')
+
+
+SELECT Buyers.FirstName As BFirstname, Buyers.LastName As BLastname, Salers.FirstName As SFirstname, Salers.LastName As SLastname, MoneySum, Date 
+FROM Sales, Salers, Buyers 
+WHERE Sales.SalerFk = Salers.Id AND Sales.BuyerFk = Buyers.Id
