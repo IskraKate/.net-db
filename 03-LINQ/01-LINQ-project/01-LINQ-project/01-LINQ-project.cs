d95c60d7a69de176560d942dbc085467df284792
Kate Iskra <kateiskra99@gmail.com>
2020-04-07 10:06:53 +0300

    trying to do 01-LINQ-proj

--- 03-LINQ/01-LINQ-project/01-LINQ-project/01-LINQ-project.cs
+++ 03-LINQ/01-LINQ-project/01-LINQ-project/01-LINQ-project.cs
@@ -80,31 +80,21 @@ namespace _01_LINQ_project
 
 
 
-            //DateTime dateTime1 = new DateTime(2020, 04, 07);
-            //DateTime dateTime2 = new DateTime(2020, 05, 07);
+            DateTime dateTime1 = new DateTime(2020, 04, 07);
+            DateTime dateTime2 = new DateTime(2020, 05, 07);
 
-            //var array = from d in _daysOfWork
+
+            var array = Enumerable.Range(0, 1 + dateTime2.Subtract(dateTime1).Days)
+              .Select(offset => dateTime1.AddDays(offset))
+            .ToArray();
+
+            //from d in _daysOfWork
             //            where d.Schedule.Name == _employeeName
-            //            .Select(offset =>
+            //            select (offset =>
             //            {
-            //                Enumerable.Range(0, 1 + d.Schedule.DateOut.Subtract(d.Schedule.DateIn).Days);
-            //                d.Schedule.DateIn.AddDays(offset);
-            //                return offset;
-            //            }).ToString();
-                             
-            //Enumerable.Range(0,1 + d.Schedule.DateOut.Subtract(d.Schedule.DateIn).Days)
-            //.Select(offset=> d) 
-            //Enumerable.Range(0, 1 + dateTime2.Subtract(d.).Days)
-            //.Select(offset => dateTime1.AddDays(offset))
-            //.ToArray()
-
-            var array = from d in _daysOfWork
-                        where d.Schedule.Name == _employeeName
-                        select (offset =>
-                        {
-                            Enumerable.Range(0, 1 + d.Schedule.DateOut.Subtract(d.Schedule.DateIn).Days)
-                             .Select(offset => d.Schedule.DateIn.AddDays(offset));
-                         });
+            //                Enumerable.Range(0, 1 + d.Schedule.DateOut.Subtract(d.Schedule.DateIn).Days)
+            //                 .Select(offset => d.Schedule.DateIn.AddDays(offset));
+            //             });
                         
                         
 
@@ -113,42 +103,7 @@ namespace _01_LINQ_project
             {
                 Console.WriteLine(a.ToShortDateString());
             }
-            //для ввода другого имени
 
-            //_employeeName = Console.ReadLine();
-
-            Console.WriteLine("Schedule\n");
-
-            //Синтаксис запросов
-
-            //var dates = from name in _daysOfWork
-            //            where name.Schedule.Name == _employeeName && _date != name.Schedule.DateOut
-            //            select new
-            //            {
-            //                Date = _date.AddDays(1),
-            //                Day = name.Day
-            //            };
-
-            //var res2 = from date in dates
-            //           where date.Date.DayOfWeek == date.Day
-            //           select date;
-
-            //foreach (var item in res2)
-            //{
-            //    Console.WriteLine($"{item.Date}");
-            //}
-
-            
-            //Синтаксис методов
-
-            //var res2 = _daysOfWork
-            //           .Where(name => name.Schedule.Name == _employeeName)
-            //           .Select(name => name);
-
-            //foreach (var item in res2)
-            //{
-            //    Console.WriteLine($"{item.Day}\n{item.Schedule.Name}\n{item.Schedule.DateIn.ToShortDateString()}\n{item.Schedule.DateOut.ToShortDateString()}\n");
-            //}
         }
     }
 }
